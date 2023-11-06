698ffc0d0367712fc3aed346d35374001a71cfd1
Logan Wood <lgnwood@gmail.com>
2023-11-05 17:55:41 -0700

    complete tests

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   };
+  
+//   // method: "POST" is missing from the object below
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json",
+//     },
+//     body: JSON.stringify(formData),
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     })
+//     .catch(function (error) {
+//       alert("Bad things! Ragnarők!");
+//       console.log(error.message);
+//     });
+
+
+
+function submitData(userName, userEmail) {
+    const formData = {
+        name: userName,
+        email: userEmail 
+    }
+    
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+    return fetch("http://localhost:3000/users", configObj)
+    .then(function (response) {
+        return response.json();
+        })
+        .then(function (object) {
+            
+            document.body.innerHTML = object.id       
+             
+        })
+        .catch(function (error) {
+            alert("Unauthorized Access");
+            document.body.innerHTML = error.message;
+        });
+}
